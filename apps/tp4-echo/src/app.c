d44d54b5a5d3ccc65cd65213effc95819a031f34
daiana1997 <daianaaye0315@gmail.com>
2024-12-05 15:38:14 -0300

    tp4 ejercicios 3 y 4

---
+++ apps/tp4-echo/src/app.c
@@ -0,0 +1,52 @@
+/*
+ * main.c
+ *
+ *  Created on: 13 oct. 2022
+ *      Author: fep
+ */
+
+#include "../../tp4-echo/inc/app.h"         // <= Su propia cabecera (opcional)
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "sapi.h"        // <= Biblioteca sAPI
+#include "semphr.h"
+
+
+#define mainMAX_MSG_LEN 25
+
+int main( void )
+{
+		char cMessage[ mainMAX_MSG_LEN ];
+		int messageIndex;
+		uint8_t receivedByte;
+		bool_t input;
+
+        uartConfig( UART_USB, 9600);
+
+        // Inicializar y configurar la placa de desarrollo
+        boardConfig();
+
+        while (TRUE) {
+
+                	input = uartReadByte( UART_USB, &receivedByte);
+                	if (input == FALSE)
+                		continue;
+
+                	cMessage[messageIndex++] = receivedByte;
+                	if (receivedByte == '\r') {
+                		cMessage[messageIndex++] = '\0\n';
+                		uartWriteString(UART_USB, cMessage);
+                		messageIndex = 0;
+                		for (int i = 0; i < 25; i++) {
+                			cMessage[i] = 0;
+                		}
+                	}
+
+                }
+
+
+
+        for(;;);
+}
+
